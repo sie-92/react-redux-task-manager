5d13cc08b163c14aa4eb4425ea78f12207105541
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-16 23:55:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// app/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from '../features/tasks/tasksSlice';
-
-export default configureStore({
-  reducer: {
-    tasks: tasksReducer,
-  },
-});
